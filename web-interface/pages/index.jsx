85dbd49512441ccbc978fc21927706b285d61995
Stefan Kracht <stefankracht@gmail.com>
2021-02-16 18:00:38 +0100

    render scene with triangle

--- web-interface/pages/index.jsx
+++ web-interface/pages/index.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import * as THREE from "three";
+
+import React, { useRef } from "react";
+
+import { Canvas } from "react-three-fiber";
+import Grid from "../components/Grid";
+import Triangle from "../components/Triangle";
+import dynamic from "next/dynamic";
+
+const Controls = dynamic(() => import("../components/Controls"), {
+  ssr: false,
+});
+
 
 const Home = () => (
-  <h1>Test</h1>
+  <Canvas
+    camera={{ position: [100, 100, -100] }}
+    onCreated={({ gl }) => gl.setClearColor("#f0f0f0")}
+  >
+    <ambientLight intensity={0.5} />
+    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+    <pointLight position={[-10, -10, -10]} />
+    <Triangle
+      vertices={[
+        new THREE.Vector3(-20, 0, 0),
+        new THREE.Vector3(20, 0, 0),
+        new THREE.Vector3(0, -50, 0),
+      ]}
+    />
+    <Grid rotation={{ z: -90 }} />
+    <Controls />
+  </Canvas>
 );
 
 export default Home;
