f0f6c871c4208aca6816a5b4babcedf60acc19c5
Stefan Kracht <stefankracht@gmail.com>
2021-02-19 18:03:53 +0100

    add getLenghtByPoints to model

--- web-interface/pages/index.jsx
+++ web-interface/pages/index.jsx
@@ -7,6 +7,7 @@ import Grid from "../components/Grid";
 import Triangle from "../components/Triangle";
 import config from "../config";
 import dynamic from "next/dynamic";
+import { getLenghtByPoints } from "../../lib/plotter-model";
 import { useSpring } from "react-spring/three.cjs";
 
 const Controls = dynamic(() => import("../components/Controls"), {
@@ -17,14 +18,13 @@ const defaultUpperLeft = [-config.cylinder.distance / 2, 0, 0];
 const defaultUpperRight = [config.cylinder.distance / 2, 0, 0];
 const defaultPenPosition = [0, -config.pen.topDistance, 0];
 
-const getLenghtByPoints = ([x1, y1], [x2, y2]) =>
-  Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
-
 const Home = () => {
   const [upperLeft, setUpperLeft] = useState(defaultUpperLeft);
   const [upperRight, setUpperRight] = useState(defaultUpperRight);
 
   const [penPosition, setPenPosition] = useState(defaultPenPosition);
+
+  getLenghtByPoints(upperLeft, penPosition);
 
   useSpring({
     from: {
@@ -40,20 +40,22 @@ const Home = () => {
   });
 
   return (
-    <Canvas camera={{ position: [-20, 0, -75] }}>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Triangle
-        vertices={[
-          new THREE.Vector3(...upperLeft),
-          new THREE.Vector3(...upperRight),
-          new THREE.Vector3(...penPosition),
-        ]}
-      />
-      <Grid rotation={{ z: -90 }} />
-      <Controls />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [-20, 0, -75] }}>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Triangle
+          vertices={[
+            new THREE.Vector3(...upperLeft),
+            new THREE.Vector3(...upperRight),
+            new THREE.Vector3(...penPosition),
+          ]}
+        />
+        <Grid rotation={{ z: -90 }} />
+        <Controls />
+      </Canvas>
+    </>
   );
 };
 
