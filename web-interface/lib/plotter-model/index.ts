6baf6156106e6e1aa7ef8ba3b9c2db9ce4d9f261
Stefan Kracht <stefankracht@gmail.com>
2021-02-23 17:16:28 +0100

    move model and use typescript

--- web-interface/lib/plotter-model/index.ts
+++ web-interface/lib/plotter-model/index.ts
@@ -1,10 +1,16 @@
 import BigDecimal from "decimal.js";
+import { parse } from "svg-parser";
 
 /**
  *  Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
  */
-export const getLenghtByPoints = ([x1, y1], [x2, y2]) => {
+export const getLenghtByPoints = (
+  [x1, y1]: [number, number],
+  [x2, y2]: [number, number]
+) => {
   const a = new BigDecimal(x1).sub(x2).abs().pow(2);
   const b = new BigDecimal(y1).sub(y2).abs().pow(2);
   return a.add(b).sqrt();
 };
+
+export const getSVGObject = (filecontent: string) => parse(filecontent);
