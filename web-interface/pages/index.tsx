35c075b0b1a6d9f542cc18e5ecfab1c7977cb9b3
Stefan Kracht <stefankracht@gmail.com>
2021-02-24 15:27:32 +0100

    move perspective

--- web-interface/pages/index.tsx
+++ web-interface/pages/index.tsx
@@ -45,17 +45,20 @@ const Home = () => {
   return (
     <>
       <Canvas camera={{ position: [-20, 0, -75] }}>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <Triangle
-          vertices={[
-            new THREE.Vector3(...upperLeft, 0),
-            new THREE.Vector3(...upperRight, 0),
-            new THREE.Vector3(...penPosition, 0),
-          ]}
-        />
-        <Grid />
+        <group position={[0, 30, 0]}>
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          <Triangle
+            vertices={[
+              new THREE.Vector3(...upperLeft, 0),
+              new THREE.Vector3(...upperRight, 0),
+              new THREE.Vector3(...penPosition, 0),
+            ]}
+          />
+
+          <Grid />
+        </group>
         <Controls />
       </Canvas>
     </>
