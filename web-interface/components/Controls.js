21a9ef0fb577e3961da44da9beee0ef71f20d021
Stefan Kracht <stefankracht@gmail.com>
2021-03-02 19:05:32 +0100

    make controls work again

--- web-interface/components/Controls.js
+++ web-interface/components/Controls.js
@@ -1,15 +1,13 @@
 import React, { useRef } from "react";
-import {
-  useFrame,
-  useThree
-} from "react-three-fiber";
+import { extend, useFrame, useThree } from "react-three-fiber";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 extend({ OrbitControls });
 
-const Controls= () => {
+const Controls = () => {
   const ref = useRef();
   const { camera, gl } = useThree();
-
   useFrame(() => {
     if (ref && ref.current) {
       ref.current.update();
