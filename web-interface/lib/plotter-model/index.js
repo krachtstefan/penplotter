530b3e56e7a3a322e691e07f25dc5245ab960060
Stefan Kracht <stefankracht@gmail.com>
2021-03-04 20:12:48 +0100

    export getLenghtByPoints again

--- web-interface/lib/plotter-model/index.js
+++ web-interface/lib/plotter-model/index.js
@@ -1,15 +1,6 @@
 import BigDecimal from "decimal.js";
 import { parse } from "svg-parser";
 
-// /**
-//  *  Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
-//  */
-// export const getLenghtByPoints = ([x1, y1], [x2, y2]) => {
-//   const a = new BigDecimal(x1).sub(x2).abs().pow(2);
-//   const b = new BigDecimal(y1).sub(y2).abs().pow(2);
-//   return a.add(b).sqrt();
-// };
-
 class PenPlotter {
   constructor(svg) {
     const svgObj = parse(svg);
@@ -28,5 +19,14 @@ class PenPlotter {
     return getChildren(obj).flat(99);
   };
 }
+
+/**
+ *  Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
+ */
+export const getLenghtByPoints = ([x1, y1], [x2, y2]) => {
+  const a = new BigDecimal(x1).sub(x2).abs().pow(2);
+  const b = new BigDecimal(y1).sub(y2).abs().pow(2);
+  return a.add(b).sqrt();
+};
 
 export default PenPlotter;
