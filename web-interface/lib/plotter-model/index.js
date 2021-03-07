3cce92a94628dd4d5c57241c9ecaa2a943e4b8a0
Stefan Kracht <stefankracht@gmail.com>
2021-03-07 10:58:45 +0100

    update getDimensions

--- web-interface/lib/plotter-model/index.js
+++ web-interface/lib/plotter-model/index.js
@@ -69,9 +69,9 @@ export const returnPointsFromElement = (element) => {
   }
 };
 
-export const getDimensions = (arrOfPoints) => {
-  const allX = arrOfPoints.map((p) => p[0]);
-  const allY = arrOfPoints.map((p) => p[1]);
+export const getDimensions = (...arrOfPointArray) => {
+  const allX = arrOfPointArray.flat().map((p) => p[0]);
+  const allY = arrOfPointArray.flat().map((p) => p[1]);
   return {
     width: Math.max(...allX) - Math.min(...allX),
     height: Math.max(...allY) - Math.min(...allY),
