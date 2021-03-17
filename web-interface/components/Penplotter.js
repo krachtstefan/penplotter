bac74c35d04411deb2060540ffed7836cb470e90
Stefan Kracht <stefankracht@gmail.com>
2021-03-17 10:19:24 +0100

    move group

--- web-interface/components/Penplotter.js
+++ web-interface/components/Penplotter.js
@@ -73,7 +73,7 @@ const Penplotter = () => {
   return (
     <>
       <Canvas camera={{ position: [20 * 10, 0, 75 * 10] }}>
-        <group position={[0, 0, 0]}>
+        <group position={[0, config.paper.height, 0]}>
           <Paper
             width={config.paper.width}
             height={config.paper.height}
