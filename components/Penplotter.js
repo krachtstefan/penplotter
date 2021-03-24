5022e0d3d6760c699f20421b6ff549d38f31659c
Stefan Kracht <stefankracht@gmail.com>
2021-03-24 10:18:52 +0100

    add controls

--- components/Penplotter.js
+++ components/Penplotter.js
@@ -18,6 +18,7 @@ import Material from "./Material";
 import React from "react";
 import config from "../config";
 import dynamic from "next/dynamic";
+import { useControls } from "leva";
 
 const svgFile = preval`module.exports = require("fs").readFileSync("./assets/example.svg", "utf8")`;
 const parsedSvg = new PenPlotter(svgFile);
@@ -30,49 +31,84 @@ const elementsToDraw = parsedSvg
   .flat();
 
 const { width, height } = getDimensions(elementsToDraw);
-const scaling = Math.min(
-  ...[
-    new BigDecimal(config.paper.width).div(new BigDecimal(width)).toNumber(),
-    new BigDecimal(config.paper.height).div(new BigDecimal(height)).toNumber(),
-  ]
-);
-
-// add projection
-const mirroredY = mirrorY(elementsToDraw);
-const scaled = scale(mirroredY, scaling);
-const { top, left } = getPosition(scaled);
-const { width: scaledWidth, height: scaledHeight } = getDimensions(scaled);
-
-const moved = move(scaled, {
-  top:
-    -top - config.paper.topDistance - (config.paper.height - scaledHeight) / 2,
-  left: -left - config.paper.width / 2 + (config.paper.width - scaledWidth) / 2,
-});
-
-const upperLeft = [-config.cylinder.distance / 2, 0];
-const upperRight = [config.cylinder.distance / 2, 0];
-
-const penPositions = moved.flat();
-
-const lengthSequence = penPositions.map((pos) => [
-  getLenghtByPoints(upperLeft, pos),
-  getLenghtByPoints(upperRight, pos),
-]);
-
-const lengthChangeSequence = lengthSequence.map((co, i, srcArr) =>
-  i > 0
-    ? [srcArr[i - 1][0].minus(co[0]), srcArr[i - 1][1].minus(co[1])]
-    : [new BigDecimal(0), new BigDecimal(0)]
-);
-
-const rotationDegSequence = lengthChangeSequence.map((x) => [
-  x[0].div(config.cylinder.circumference).times(360).toNumber(),
-  x[1].div(config.cylinder.circumference).times(360).toNumber(),
-]);
-
-console.log(rotationDegSequence);
 
 const Penplotter = () => {
+  const { gridEnabled } = useControls("Settings", {
+    gridEnabled: {
+      value: true,
+      label: "grid",
+    },
+  });
+  const { cylinderDistance } = useControls("Cylinder", {
+    cylinderDistance: {
+      value: config.cylinder.distance,
+      min: 0,
+      max: 1000,
+      label: "distance",
+    },
+  });
+  const { paperWidth, paperHeight, paperTopDistance } = useControls("Paper", {
+    paperWidth: {
+      value: config.paper.width,
+      min: 0,
+      max: 500,
+      label: "width",
+    },
+    paperHeight: {
+      value: config.paper.height,
+      min: 0,
+      max: 500,
+      label: "height",
+    },
+    paperTopDistance: {
+      value: config.paper.topDistance,
+      min: 0,
+      max: 500,
+      label: "distance",
+    },
+  });
+
+  const scaling = Math.min(
+    ...[
+      new BigDecimal(paperWidth).div(new BigDecimal(width)).toNumber(),
+      new BigDecimal(paperHeight).div(new BigDecimal(height)).toNumber(),
+    ]
+  );
+
+  // add projection
+  const mirroredY = mirrorY(elementsToDraw);
+  const scaled = scale(mirroredY, scaling);
+  const { top, left } = getPosition(scaled);
+  const { width: scaledWidth, height: scaledHeight } = getDimensions(scaled);
+
+  const moved = move(scaled, {
+    top: -top - paperTopDistance - (paperHeight - scaledHeight) / 2,
+    left: -left - paperWidth / 2 + (paperWidth - scaledWidth) / 2,
+  });
+
+  const upperLeft = [-cylinderDistance / 2, 0];
+  const upperRight = [cylinderDistance / 2, 0];
+
+  const penPositions = moved.flat();
+
+  const lengthSequence = penPositions.map((pos) => [
+    getLenghtByPoints(upperLeft, pos),
+    getLenghtByPoints(upperRight, pos),
+  ]);
+
+  const lengthChangeSequence = lengthSequence.map((co, i, srcArr) =>
+    i > 0
+      ? [srcArr[i - 1][0].minus(co[0]), srcArr[i - 1][1].minus(co[1])]
+      : [new BigDecimal(0), new BigDecimal(0)]
+  );
+
+  const rotationDegSequence = lengthChangeSequence.map((x) => [
+    x[0].div(config.cylinder.circumference).times(360).toNumber(),
+    x[1].div(config.cylinder.circumference).times(360).toNumber(),
+  ]);
+
+  console.log(rotationDegSequence);
+
   const { penPositionX, penPositionY } = useSpring({
     from: {
       penPositionX: [penPositions[0][0]],
@@ -93,7 +129,7 @@ const Penplotter = () => {
         camera={{ position: [20 * 10, 0, 75 * 10] }}
         onCreated={({ gl }) => gl.setClearColor("white")}
       >
-        <group position={[0, config.paper.height, 0]}>
+        <group position={[0, paperHeight, 0]}>
           <Material
             width={config.board.width}
             height={config.board.height}
@@ -102,9 +138,9 @@ const Penplotter = () => {
             color={"#e1e4e8"}
           />
           <Material
-            width={config.paper.width}
-            height={config.paper.height}
-            center={[0, -config.paper.height / 2 - config.paper.topDistance, 0]}
+            width={paperWidth}
+            height={paperHeight}
+            center={[0, -paperHeight / 2 - paperTopDistance, 0]}
             zPosition={-1}
             color={"white"}
           />
@@ -131,7 +167,7 @@ const Penplotter = () => {
             <lineBasicMaterial attach="material" color="#444c56" />
           </animated.line>
 
-          <Grid />
+          {gridEnabled ? <Grid /> : null}
         </group>
         <Controls />
       </Canvas>
