d0fa1d81fe6421cf4554b53cde63c14d35985402
Stefan Kracht <stefankracht@gmail.com>
2021-04-12 18:43:55 +0200

    add hardware config file

---
+++ hardware/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  hardware: {
+    pen: {
+      pin: 11,
+      durationUp: 1000,
+      durationDown: 2000,
+    },
+    stepper: {
+      stepsPerRotation: 1600,
+      pauseBetweenInstructions: 500,
+    },
+  },
+};
