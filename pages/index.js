61edfcb004a49078b7a965f845262083d6dfa849
Stefan Kracht <stefankracht@gmail.com>
2021-04-14 10:05:15 +0200

    websocket handshake test

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
+import config from "../config";
 import dynamic from "next/dynamic";
 
 const PenPlotter = dynamic(() => import("../components/PenPlotter"), {
   ssr: false,
 });
 
-const Home = () => (
-  <>
-    <PenPlotter />
-  </>
-);
+const Home = () => {
+  const socketUrl = config.websocket.address;
+
+  const {
+    sendJsonMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket,
+  } = useWebSocket(socketUrl, {
+    onOpen: () => console.log("opened"),
+    onClose: () => console.log("closed"),
+    onError: () => console.log("error"),
+    shouldReconnect: () => true,
+  });
+
+  const connectionStatus = {
+    [0]: "Connecting",
+    [1]: "Open",
+    [2]: "Closing",
+    [3]: "Closed",
+    [-1]: "Uninstantiated",
+  }[readyState];
+
+  console.log(
+    "connectionStatus",
+    connectionStatus,
+    "lastJsonMessage",
+    lastJsonMessage
+  );
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          sendJsonMessage({ cool: "message" });
+        }}
+      >
+        Send Message
+      </button>
+      <PenPlotter />
+    </>
+  );
+};
 
 export default Home;
