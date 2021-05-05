e0ece301928dacd1a059b670d1bd61a4c6f4091f
Stefan Kracht <stefankracht@gmail.com>
2021-05-05 17:33:12 +0200

    add imports

--- hardware/src/index.ts
+++ hardware/src/index.ts
@@ -1,6 +1,20 @@
-import { Board, Led } from "johnny-five";
+import { Board, Servo, Stepper } from "johnny-five";
+import {
+  addDrawingJob,
+  finishPenMovement,
+  startDrawing,
+  startPenMovement,
+  stopDrawing,
+  updateProgress,
+} from "./redux/penplotter";
+import { populate, wss } from "./websockets";
+
+import BigDecimal from "decimal.js";
+import { PenState } from "./redux/penplotter/types";
+import devices from "./devices";
+import hardware from "./config";
+import moment from "moment";
+import store from "./redux";
+
 const board = new Board();
-board.on("ready", () => {
-  const led = new Led(13);
-  led.blink(500);
-});
+board.on("ready", () => {});
