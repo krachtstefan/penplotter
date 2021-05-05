a7eeb8803ea1a1a6da7e94249ef95ecf4569b2c3
Stefan Kracht <stefankracht@gmail.com>
2021-05-05 18:33:29 +0200

    delete build

--- hardware/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var johnny_five_1 = require("johnny-five");
-var board = new johnny_five_1.Board();
-board.on("ready", function () {
-    var led = new johnny_five_1.Led(13);
-    led.blink(500);
-});
