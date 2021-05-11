f4e7e34dac685dfccd1597f0b63887897b2d2771
Stefan Kracht <stefankracht@gmail.com>
2021-05-11 20:12:55 +0200

    fix material

--- components/PlotterPreview/Material.tsx
+++ components/PlotterPreview/Material.tsx
@@ -1,45 +1,17 @@
-import * as THREE from "three";
+import React from "react";
 
-import React, { useMemo } from "react";
-
-const Paper = ({
+const Material = ({
   width = 20,
   height = 20,
   center = [0, 0, 0],
-  zPosition = 0,
   color = "white",
 }): React.FC => {
-  const f32array = useMemo(() => {
-    const vertices = [
-      new THREE.Vector3(-width / 2, -height / 2, zPosition),
-      new THREE.Vector3(width / 2, -height / 2, zPosition),
-      new THREE.Vector3(width / 2, height / 2, zPosition),
-      new THREE.Vector3(width / 2, height / 2, zPosition),
-      new THREE.Vector3(-width / 2, height / 2, zPosition),
-      new THREE.Vector3(-width / 2, -height / 2, zPosition),
-    ];
-
-    return Float32Array.from(
-      new Array(vertices.length).fill().flatMap((_, i) => vertices[i].toArray())
-    );
-  }, [width, height]);
-
   return (
     <mesh position={center}>
-      <bufferGeometry attach="geometry">
-        <bufferAttribute
-          attachObject={["attributes", "position"]}
-          args={[f32array, 3]}
-        />
-      </bufferGeometry>
-      <meshBasicMaterial
-        attach="material"
-        color={color}
-        wireframe={false}
-        side={THREE.DoubleSide}
-      />
+      <planeBufferGeometry attach="geometry" args={[width, height]} />
+      <meshToonMaterial attach="material" color={color} />
     </mesh>
   );
 };
 
-export default Paper;
+export default Material;
