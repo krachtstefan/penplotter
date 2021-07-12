e052f7e607af6ed340cb3efe0c4d1c751efa6a12
Stefan Kracht <stefankracht@gmail.com>
2021-07-12 18:10:29 +0200

    sort math

--- lib/plotter-model/math.ts
+++ lib/plotter-model/math.ts
@@ -1,6 +1,14 @@
 import BigDecimal from "decimal.js";
 import { Point2D } from "./types";
 
+export const createPoint2D = (arr: [any, any]): Point2D => [
+  new BigDecimal(arr[0]),
+  new BigDecimal(arr[1]),
+];
+
+export const arrayofNumberArrToPoint2D = (arr: [any, any][]) =>
+  arr.map((numericArr) => createPoint2D(numericArr));
+
 /**
  * example:
  *
@@ -27,19 +35,6 @@ export const convertPointsRelToAbs = (
     .slice(1);
 
 /**
- * returns a point from the line segment between start and finish
- * at the given fraction. A fraction of 0.5 f.e returns the midpoint
- */
-export const getPointFromLineSegment = (
-  start: Point2D,
-  finish: Point2D,
-  fraction: number
-): Point2D => [
-  start[0].minus(finish[0]).times(-fraction).plus(start[0]),
-  start[1].minus(finish[1]).times(-fraction).plus(start[1]),
-];
-
-/**
  *  Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
  */
 export const getLenghtByPoints = (
@@ -51,57 +46,18 @@ export const getLenghtByPoints = (
   return a.add(b).sqrt();
 };
 
-export const createPoint2D = (arr: [any, any]): Point2D => [
-  new BigDecimal(arr[0]),
-  new BigDecimal(arr[1]),
+/**
+ * returns a point from the line segment between start and finish
+ * at the given fraction. A fraction of 0.5 f.e returns the midpoint
+ */
+export const getPointFromLineSegment = (
+  start: Point2D,
+  finish: Point2D,
+  fraction: number
+): Point2D => [
+  start[0].minus(finish[0]).times(-fraction).plus(start[0]),
+  start[1].minus(finish[1]).times(-fraction).plus(start[1]),
 ];
-
-export const arrayofNumberArrToPoint2D = (arr: [any, any][]) =>
-  arr.map((numericArr) => createPoint2D(numericArr));
-
-/**
- *
- * The quadratic bezier curve has three points, a start, finish and a control point
- * there a three interploations done at the same time, to get a point (C) on that curve
- * 1: a point (A) on the line from the start to the control point
- * 2: a point (B) on the line from the control point to the finish
- * 3: a point (C) on the line from point A to B
- *
- * It's much simpler as it sounds: https://youtu.be/pnYccz1Ha34
- */
-export const quadraticBezier = (
-  [start, controlPoint, finish]: [Point2D, Point2D, Point2D],
-  fraction: number
-) =>
-  getPointFromLineSegment(
-    getPointFromLineSegment(start, controlPoint, fraction),
-    getPointFromLineSegment(controlPoint, finish, fraction),
-    fraction
-  );
-
-/**
- *
- * The cubic bezier curve has four points, a start, finish and control point a and b
- * it is basically an interpolation between two quadratic bezier curves one with start,
- * control point a and control point b and one with control point a, control point b
- * and the finish point
- *
- * Again, it's not that complicated: https://youtu.be/pnYccz1Ha34
- */
-export const cubicBezier = (
-  [start, controlPointA, controlPointB, finish]: [
-    Point2D,
-    Point2D,
-    Point2D,
-    Point2D
-  ],
-  fraction: number
-) =>
-  getPointFromLineSegment(
-    quadraticBezier([start, controlPointA, controlPointB], fraction),
-    quadraticBezier([controlPointA, controlPointB, finish], fraction),
-    fraction
-  );
 
 export const getPosition = (arrOfPoints: Point2D[]): Point2D => {
   const allX = arrOfPoints.map((p) => p[0].toNumber());
@@ -150,3 +106,47 @@ export const move = (
     new BigDecimal(x).add(right || 0),
     new BigDecimal(y).add(down || 0),
   ]);
+
+/**
+ *
+ * The quadratic bezier curve has three points, a start, finish and a control point
+ * there a three interploations done at the same time, to get a point (C) on that curve
+ * 1: a point (A) on the line from the start to the control point
+ * 2: a point (B) on the line from the control point to the finish
+ * 3: a point (C) on the line from point A to B
+ *
+ * It's much simpler as it sounds: https://youtu.be/pnYccz1Ha34
+ */
+export const quadraticBezier = (
+  [start, controlPoint, finish]: [Point2D, Point2D, Point2D],
+  fraction: number
+) =>
+  getPointFromLineSegment(
+    getPointFromLineSegment(start, controlPoint, fraction),
+    getPointFromLineSegment(controlPoint, finish, fraction),
+    fraction
+  );
+
+/**
+ *
+ * The cubic bezier curve has four points, a start, finish and control point a and b
+ * it is basically an interpolation between two quadratic bezier curves one with start,
+ * control point a and control point b and one with control point a, control point b
+ * and the finish point
+ *
+ * Again, it's not that complicated: https://youtu.be/pnYccz1Ha34
+ */
+export const cubicBezier = (
+  [start, controlPointA, controlPointB, finish]: [
+    Point2D,
+    Point2D,
+    Point2D,
+    Point2D
+  ],
+  fraction: number
+) =>
+  getPointFromLineSegment(
+    quadraticBezier([start, controlPointA, controlPointB], fraction),
+    quadraticBezier([controlPointA, controlPointB, finish], fraction),
+    fraction
+  );
