eb721adb23812f55c4afdc5a71389de4fb8c3758
Stefan Kracht <stefankracht@gmail.com>
2021-07-13 15:48:17 +0200

    user everything.svg

--- components/PlotterPreview/index.tsx
+++ components/PlotterPreview/index.tsx
@@ -28,7 +28,7 @@ import config from "../../config";
 import dynamic from "next/dynamic";
 import useWebSocket from "react-use-websocket";
 
-const svgFile = preval`module.exports = require("fs").readFileSync("./assets/examples/line.svg", "utf8")`;
+const svgFile = preval`module.exports = require("fs").readFileSync("./assets/examples/everything.svg", "utf8")`;
 const parsedSvg = new SvgParser(svgFile);
 
 const Controls = dynamic(() => import("./Controls"), { ssr: false });
